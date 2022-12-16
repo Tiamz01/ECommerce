be6018c3d77f34804d712e35165e4b9a422e6a28
Ismail Tiamiyu <tiami.abiola@gmail.com>
2022-12-16 22:12:46 +0100

    commiting the cart update state  section

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,15 @@ import App from "./App";
 import Store from "./app/Store.js";
 import "./index.css";
 Store;
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={Store}>
+			<Toaster
+				position='top-center'
+				reverseOrder={false}
+			/>
 			<App />
 		</Provider>
 	</React.StrictMode>
