05c564093da3b817a95c045af137839e685138d8
tiamz <tiami.abiola@gmail.com>
2025-10-05 18:09:28 +0100

    perf: Finalize 10-second hover animations for cinematic effect

--- src/component/Flexcontent.jsx
+++ src/component/Flexcontent.jsx
@@ -42,11 +42,11 @@ const FlexContent = ({
 					<img
 						src={img}
 						alt={`img/${heading}`}
-						className={`transitions-theme-slow w-auto object-fill mt-7 ${
+						className={`transitions-theme w-auto object-fill mt-7 ${
 							ifExists
-								? "h-60 lg:h-56 md:h-52 sm:h-44 xsm:h-36 rotate-6 hover:rotate-9  "
-								: " h-72 lg:h-64 md:h-60 sm:h-48 xsm:h-40 rotate-[19deg] hover:rotate-[22deg]"
-						} transition-transform duration-[10000ms] ease-in-out`}
+								? "h-60 lg:h-56 md:h-52 sm:h-44 xsm:h-36 rotate-6 hover:rotate-12  "
+								: " h-72 lg:h-64 md:h-60 sm:h-48 xsm:h-40 rotate-[19deg] hover:rotate-1"
+						}`}
 					/>
 				</div>
 			</div>
